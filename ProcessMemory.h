d27ba3cf901eb24e131883f7b52f9d4f22890e8c
Viktor Horsmanheimo <viktor.horsmanheimo@gmail.com>
2025-06-05 18:29:20 +0200

    Rework ProcessMemory
    - Use vectors over sets to store addresses
    - Do not search the full memory range after
      the initial search
    - Reduce the complexity of
      ProcessesMemory::read_process_memory

--- ProcessMemory.h
+++ ProcessMemory.h
@@ -5,20 +5,16 @@
 #include <variant>
 #include <memory>
 #include <unordered_set>
-#include <set>
-#include <sys/mman.h>
-#include <sys/uio.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <limits>
 #include <cstdio>
 #include <iostream>
 
 #include "maps.h"
 
 
-using Match = std::variant<std::uint8_t*, std::uint16_t*, std::uint32_t*, std::int32_t*>;
-using MatchSet = std::set<Match>;
+using Match = std::variant<std::uint8_t *, std::uint16_t *, std::uint32_t *>;
+using MatchSet = std::unordered_set<Match>;
 
 /*
  * maybe make this into a class? you could place the pid into
@@ -26,13 +22,14 @@ using MatchSet = std::set<Match>;
  */
 class ProcessMemory {
     pid_t _pid{};
-    MatchSet matches;
+    std::vector<Match> matches;
 
 public:
     ssize_t write_process_memory(void *address, void *buffer, const ssize_t n) const;
+
     ssize_t read_process_memory(void *address, void *buffer, size_t n) const;
 
-    MatchSet &get_matches() {
+    decltype(matches) &get_matches() {
         return matches;
     }
 
@@ -50,38 +47,38 @@ public:
      * @param value value to look for
      * @return
      */
-    template <typename T>
-    std::vector<T*> scan_range(const address_range &range, const T value) {
+    template<typename T>
+    std::vector<T *> scan_range(const address_range &range, const T value) {
         // We're largely dependent on how large the page size is on how much we can actually read.
         const std::size_t max_size = IOV_MAX * sysconf(_SC_PAGESIZE);
         const std::size_t increments = max_size / sizeof(T);
 
-        std::vector<T*> found;
+        std::vector<T *> found;
 
-        std::unique_ptr<T[]> buf(new  (std::align_val_t(64)) T[increments]);
+        std::unique_ptr<T[]> buf(new(std::align_val_t(64)) T[increments]);
         char *end = static_cast<char *>(range.start) + range.length;
 
-        for (char  *start = static_cast<char *>(range.start);
-                    start < end;
-                    start += increments) {
+        for (char *start = static_cast<char *>(range.start);
+             start < end;
+             start += increments) {
             std::uintptr_t ptrdiff = end - start;
-            std::size_t size = std::min(max_size, ptrdiff);
+            ssize_t size = std::min(max_size, ptrdiff);
 
             ssize_t nread = read_process_memory(start, buf.get(), size);
             if (nread < 0 && nread == size) {
                 std::fprintf(
                     stderr,
                     "ProcessMemory: Error reading memory at %p: %s\n",
-                    static_cast<void*>(start),
+                    static_cast<void *>(start),
                     std::strerror(errno)
                 );
                 return found;
             }
 
-            for (int i = 0; i < size / sizeof(T); i++) {
+            for (size_t i = 0; i < size / sizeof(T); i++) {
                 if (buf[i] == value) {
-                    assert(start + i < (range.start + range.length));
-                    found.push_back(reinterpret_cast<T*>(start + i * sizeof(T)));
+                    assert(start + i < (static_cast<char*>(range.start) + range.length));
+                    found.push_back(reinterpret_cast<T *>(start + i * sizeof(T)));
                 }
             }
         }
@@ -95,29 +92,62 @@ public:
      * @return returns the matches it finds
      */
     template<typename T>
-    MatchSet &scan(T value) {
-
-        std::unique_ptr<address_range> list = get_memory_ranges(_pid);
-        address_range *current = list.get();
-
-        MatchSet found;
-        /*
-         * eliminate previous matches, check what the pointer in previous_matches points to and make sure it points to `value`
-         * and that it's in current matches
-         * TODO:
-         * figure out how to quickly check the memory addresses that are in previous matches, the fastest way can't be to use process_vm_readv
-         * */
+    decltype(matches) &scan(T value) {
         using Clock = std::chrono::high_resolution_clock;
 
         auto overall_t0 = Clock::now();
 
+        if (matches.size() == 0) {
+            matches = initial_scan<T>(value);
+        } else {
+            scan_found<T>(value);
+        }
+
+        auto overall_t1 = Clock::now();
+        std::chrono::duration<double> total_elapsed = overall_t1 - overall_t0;
+        double total_seconds = total_elapsed.count();
+        fprintf(stdout, "Total scan time: %.3f s\n", total_seconds);
+        fprintf(stdout, "matches: %lu\n", matches.size());
+        return matches;
+    }
+
+private:
+    template<typename T>
+    void scan_found(T value) {
+        for (std::size_t i = 0; i < matches.size(); ++i) {
+            auto &match = matches[i];
+            std::visit([&]<typename U>(U *addr) {
+                if constexpr (std::is_same_v<T, U>) {
+                    if (addr == nullptr) {
+                        return;
+                    }
+                    U new_value{};
+                    [[maybe_unused]] ssize_t n = read_process_memory(addr, &new_value, sizeof(new_value));
+                    assert(n == sizeof(T));
+                    if (new_value != value) {
+                        matches[i] = static_cast<T *>(nullptr);
+                    }
+                }
+            }, match);
+        }
+    }
+
+    template<typename T>
+    std::vector<Match> initial_scan(T value) {
+        using Clock = std::chrono::high_resolution_clock;
+        std::unique_ptr<address_range> list = get_memory_ranges(_pid);
+        address_range *current = list.get();
+        std::vector<Match> found;
+
         while (current != nullptr) {
-            if (!(current->perms & PERM_EXECUTE) && current->perms & PERM_READ) {
+            // Do not scan vvar & vvar_clock, not permitted.
+            if (!(current->perms & PERM_EXECUTE) && current->perms & PERM_READ &&
+                std::strncmp(current->name, "[vvar]", 4096) && std::strncmp(current->name, "[vvar_vclock]", 4096)) {
                 fprintf(stdout,
                         "Scanning %s: %p - %p (size: %zx)\n",
                         current->name,
                         current->start,
-                        static_cast<char*>(current->start) + current->length,
+                        static_cast<char *>(current->start) + current->length,
                         current->length);
 
                 const size_t region_bytes = current->length;
@@ -125,39 +155,20 @@ public:
 
                 auto t0 = Clock::now();
                 auto current_matches = scan_range<T>(*current, value);
-                found.insert(current_matches.begin(), current_matches.end());
+                found.insert(found.end(), current_matches.begin(), current_matches.end());
 
                 auto t1 = Clock::now();
 
                 std::chrono::duration<double> elapsed = t1 - t0;
                 double seconds = elapsed.count();
-
-                double rate = 0.0;
-                if (seconds > 0.0) {
-                    rate = static_cast<double>(num_addresses) / seconds;
-                }
-
                 fprintf(stdout,
-                        "    → Scanned %zu addresses in %.5f s (%.0f addresses/s)\n",
+                        "    → Scanned %zu addresses in %.5f s\n",
                         num_addresses,
-                        seconds,
-                        rate);
-
+                        seconds);
             }
 
             current = current->next.get();
         }
-
-        if (!matches.empty()) {
-            std::erase_if(matches, [&](auto const &x) { return found.find(x) == found.end(); });
-        } else {
-            matches = found;
-        }
-        auto overall_t1 = Clock::now();
-        std::chrono::duration<double> total_elapsed = overall_t1 - overall_t0;
-        double total_seconds = total_elapsed.count();
-        fprintf(stdout, "Total scan time: %.3f s\n", total_seconds);
-        fprintf(stdout, "matches: %lu\n", matches.size());
-        return matches;
+        return found;
     }
 };
