a5a180c906f87359c2ac0c707d430e191dc8357f
Viktor Horsmanheimo <viktor.horsmanheimo@gmail.com>
2025-06-05 18:15:58 +0200

    Register Match as a metatype

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 
 #include <QApplication>
 
+Q_DECLARE_METATYPE(Match);
 int main(int argc, char *argv[])
 {
+    qRegisterMetaType<Match>("Match");
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
